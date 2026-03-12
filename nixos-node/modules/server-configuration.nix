7900bdfb956d440b4677c113ad4da7a566d25f87
CodeMaster4711 <cedi27@t-online.de>
2026-03-12 20:06:23 +0100

    test: new test release version

--- nixos-node/modules/server-configuration.nix
+++ nixos-node/modules/server-configuration.nix
@@ -158,7 +158,7 @@ services:
     restart: unless-stopped
 
   api-gateway:
-    image: ghcr.io/cs-foundry/csf-ce-api-gateway:0.2.2-alpha.371
+    image: ghcr.io/csfx-cloud/csf-ce-api-gateway:0.2.2-alpha.381
     container_name: csf-api-gateway
     environment:
       DATABASE_URL: postgres://csf:csfpassword@patroni:5432/csf_core
@@ -186,7 +186,7 @@ services:
       start_period: 30s
 
   registry:
-    image: ghcr.io/cs-foundry/csf-ce-registry:0.2.2-alpha.371
+    image: ghcr.io/csfx-cloud/csf-ce-registry:0.2.2-alpha.381
     container_name: csf-registry
     environment:
       DATABASE_URL: postgres://csf:csfpassword@patroni:5432/csf_core
@@ -202,7 +202,7 @@ services:
     restart: unless-stopped
 
   scheduler:
-    image: ghcr.io/cs-foundry/csf-ce-scheduler:0.2.2-alpha.371
+    image: ghcr.io/csfx-cloud/csf-ce-scheduler:0.2.2-alpha.381
     container_name: csf-scheduler
     environment:
       DATABASE_URL: postgres://csf:csfpassword@patroni:5432/csf_core
@@ -217,7 +217,7 @@ services:
     restart: unless-stopped
 
   volume-manager:
-    image: ghcr.io/cs-foundry/csf-ce-volume-manager:0.2.2-alpha.371
+    image: ghcr.io/csfx-cloud/csf-ce-volume-manager:0.2.2-alpha.381
     container_name: csf-volume-manager
     environment:
       DATABASE_URL: postgres://csf:csfpassword@patroni:5432/csf_core
@@ -234,7 +234,7 @@ services:
     restart: unless-stopped
 
   failover-controller:
-    image: ghcr.io/cs-foundry/csf-ce-failover-controller:0.2.2-alpha.371
+    image: ghcr.io/csfx-cloud/csf-ce-failover-controller:0.2.2-alpha.381
     container_name: csf-failover-controller
     environment:
       DATABASE_URL: postgres://csf:csfpassword@patroni:5432/csf_core
@@ -250,7 +250,7 @@ services:
     restart: unless-stopped
 
   sdn-controller:
-    image: ghcr.io/cs-foundry/csf-ce-sdn-controller:0.2.2-alpha.371
+    image: ghcr.io/csfx-cloud/csf-ce-sdn-controller:0.2.2-alpha.381
     container_name: csf-sdn-controller
     environment:
       DATABASE_URL: postgres://csf:csfpassword@patroni:5432/csf_core
